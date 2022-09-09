d793ffb6074047f5c063f5a609fb77c81eebe688
Gregory Jacobs <greg@greg-jacobs.com>
2022-09-08 20:26:15 -0400

    Fix tests

--- libs/lib-c/src/lib/lib-c.ts
+++ libs/lib-c/src/lib/lib-c.ts
@@ -2,5 +2,5 @@ import { libA } from '@nx-test/lib-a';
 import { libB } from '@nx-test/lib-b';
 
 export function libC(): string {
-  return libA() + libB() + 'lib-c!!!';
+  return libA() + libB() + 'lib-c';
 }
