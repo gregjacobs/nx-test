db0aab2c185a3ea831195ff51a2d8acdd844c4ac
Gregory Jacobs <greg@greg-jacobs.com>
2022-09-08 20:21:37 -0400

    Add packages

--- apps/nx-test/src/app/app.tsx
+++ apps/nx-test/src/app/app.tsx
@@ -1,12 +1,18 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 import styles from './app.module.css';
 import NxWelcome from './nx-welcome';
+import { libA } from '@nx-test/lib-a';
+import { libB } from '@nx-test/lib-b';
+import { libC } from '@nx-test/lib-c';
 
 export function App() {
   return (
     <>
       <NxWelcome title="nx-test" />
       <div />
+      {libA()}
+      {libB()}
+      {libC()}
     </>
   );
 }
---
+++ libs/lib-c/src/lib/lib-c.ts
@@ -0,0 +1,6 @@
+import { libA } from '@nx-test/lib-a';
+import { libB } from '@nx-test/lib-b';
+
+export function libC(): string {
+  return libA() + libB() + 'lib-c!!!';
+}
